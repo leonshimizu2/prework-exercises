9e3f3eeee355f418cb0a0669c3fca88d01832724
leonshimizu2 <2Lmshimizu@gmail.com>
2025-10-24 07:31:48 +1000

    111.rb

---
+++ 111.rb
@@ -0,0 +1,9 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
