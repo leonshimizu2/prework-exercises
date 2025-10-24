90fe421404b304234ff2bd1fdb4e4e02f81ff62b
leonshimizu2 <2Lmshimizu@gmail.com>
2025-10-24 19:15:48 +1000

    complete exercise

--- 121.rb
+++ 121.rb
@@ -3,7 +3,10 @@
 # worked with before from the Ruby documentation. 
 # Write a comment for each method describing how it works in your own words.
 
+# klsdjfs dlfjsdlsdk fs
 
 # QUESTION 2
 # Write example code that crashes using an integer method from the Ruby documentation. 
 # Write a comment describing the error message in your own words.
+
+dsjflshdjfl ksd
