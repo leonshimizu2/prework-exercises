4ccbe0bf0e4be2da651ce33e764d2178da805752
leonshimizu2 <2Lmshimizu@gmail.com>
2025-10-24 19:13:27 +1000

    tst commit

---
+++ 121.rb
@@ -0,0 +1,9 @@
+# QUESTION 1
+# Write example code to demonstrate three string methods you've never 
+# worked with before from the Ruby documentation. 
+# Write a comment for each method describing how it works in your own words.
+
+
+# QUESTION 2
+# Write example code that crashes using an integer method from the Ruby documentation. 
+# Write a comment describing the error message in your own words.
